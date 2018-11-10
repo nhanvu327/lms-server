27b110263657594a655a18ae76c61865cb215c79
Nhan Vu Thanh <nhanvu327@gmail.com>
2018-11-11 03:52:13 +0700

    update server.js

--- src/server.ts
+++ src/server.ts
@@ -19,15 +19,15 @@ const options = isDev
   : undefined;
 
 const server = isDev
-  ? http.createServer(app).listen(app.get("port"))
-  : https.createServer(options, app).listen(app.get("port"), () => {
+  ? https.createServer(options, app).listen(app.get("port"), () => {
       console.log(
         "  App is running at http://localhost:%d in %s mode",
         app.get("port"),
         app.get("env")
       );
       console.log("  Press CTRL-C to stop\n");
-    });
+    })
+  : http.createServer(app).listen(app.get("port"));
 
 const peerserver = ExpressPeerServer(server, {
   debug: true,
