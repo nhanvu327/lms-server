02b4d39808da1ddec78044a174d3b3331935d9a1
Nhan Vu Thanh <nhanvu327@gmail.com>
2018-11-11 03:46:46 +0700

    update server.js

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import https from "https";
+import http from "http";
 import * as fs from "fs";
 import * as path from "path";
 import { ExpressPeerServer } from "peer";
@@ -8,21 +9,25 @@ import { ExpressPeerServer } from "peer";
  * Start Express server.
  */
 
-const options = {
-  key: fs
-    .readFileSync(path.join(__dirname, "../ssl/mockserver.key")),
-  cert: fs
-    .readFileSync(path.join(__dirname, "../ssl/mockserver.crt"))
-};
+const isDev = process.env.NODE_ENV === "development";
 
-const server = https.createServer(options, app).listen(app.get("port"), () => {
-  console.log(
-    "  App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env")
-  );
-  console.log("  Press CTRL-C to stop\n");
-});
+const options = isDev
+  ? {
+      key: fs.readFileSync(path.join(__dirname, "../ssl/mockserver.key")),
+      cert: fs.readFileSync(path.join(__dirname, "../ssl/mockserver.crt"))
+    }
+  : undefined;
+
+const server = isDev
+  ? http.createServer(app).listen(app.get("port"))
+  : https.createServer(options, app).listen(app.get("port"), () => {
+      console.log(
+        "  App is running at http://localhost:%d in %s mode",
+        app.get("port"),
+        app.get("env")
+      );
+      console.log("  Press CTRL-C to stop\n");
+    });
 
 const peerserver = ExpressPeerServer(server, {
   debug: true,
@@ -31,5 +36,3 @@ const peerserver = ExpressPeerServer(server, {
 });
 
 app.use("/peerjs", peerserver);
-
-export default server;
